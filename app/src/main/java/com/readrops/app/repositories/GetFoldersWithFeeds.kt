257156a79a134b50336a3b3d4bdc2a46816239c4
Christophe Henry <contact@c-henry.fr>
2025-02-06 11:24:28 +0100

    Simplify read and unread feed query

--- app/src/main/java/com/readrops/app/repositories/GetFoldersWithFeeds.kt
+++ app/src/main/java/com/readrops/app/repositories/GetFoldersWithFeeds.kt
@@ -3,12 +3,15 @@ package com.readrops.app.repositories
 import com.readrops.db.Database
 import com.readrops.db.entities.Feed
 import com.readrops.db.entities.Folder
+import com.readrops.db.entities.FoldersWithFeedAndUnreadCount
 import com.readrops.db.entities.OpenIn
+import com.readrops.db.entities.unbox
 import com.readrops.db.filters.MainFilter
 import com.readrops.db.queries.FeedUnreadCountQueryBuilder
 import com.readrops.db.queries.FoldersAndFeedsQueryBuilder
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 
 class GetFoldersWithFeeds(
     private val database: Database,
@@ -71,6 +74,15 @@ class GetFoldersWithFeeds(
 
             foldersWithFeeds.toSortedMap(nullsLast(Folder::compareTo))
         }
+    }
+
+
+    fun get2(
+        accountId: Int,
+        mainFilter: MainFilter,
+        hideReadFeeds: Boolean = false
+    ): Flow<FoldersWithFeedAndUnreadCount> = database.folderDao().selectFolders2(accountId).map {
+        it.unbox()
     }
 
     fun getNewItemsUnreadCount(accountId: Int, useSeparateState: Boolean): Flow<Int> =
